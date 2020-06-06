3c821744d8a0ce9828b8de6b83409f2d560b2d94
tichinose1 <tichinose1dev@gmail.com>
2020-06-07 04:39:16 +0900

    Initial Commit

---
+++ Kawaguchi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kawaguchi
+//
+//  Created by tichinose1 on 2020/06/07.
+//  Copyright © 2020 tichinose1dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
